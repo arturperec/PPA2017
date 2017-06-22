7a07d6fd459e1b30d733dc270adb3907dee905c6
Artur Perec <artur.g.perec@gmail.com>
2017-06-22 18:23:11 +0200

    Added Button and TextView, added function SendMessage

--- app/src/main/java/some/weird/ppa2017/MainActivity.java
+++ app/src/main/java/some/weird/ppa2017/MainActivity.java
@@ -2,6 +2,7 @@ package some.weird.ppa2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void SendMessage (View view)
+    {
+
+    }
+
 }
