b4648e2891fca34ec502ce8ad33140ced22827bc
Artur Perec <artur.g.perec@gmail.com>
2017-09-11 20:48:13 +0200

    Added SimpleDividerItemDecoration, ProfileActivity and Profile Fragment, changed image to fit layout, upgraded this image in activity profile and fragment profile

--- app/src/main/java/some/weird/ppafirebaselogin2017/ProfileActivity.java
+++ app/src/main/java/some/weird/ppafirebaselogin2017/ProfileActivity.java
@@ -1,8 +1,82 @@
 package some.weird.ppafirebaselogin2017;
 
+import android.os.Bundle;
+import android.support.design.internal.NavigationMenuView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 /**
  * Created by artur on 05.09.2017.
  */
 
-public class ProfileActivity {
+public class ProfileActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private Fragment fragment = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        fragmentManager = getSupportFragmentManager();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragment = new ProfileFragment();
+        fragmentTransaction.replace(R.id.main_container_wrapper, fragment);
+        fragmentTransaction.commit();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        disableNavigationViewScrollbars(navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item){
+                int id = item.getItemId();
+
+                if (id == R.id.nav_profile) {
+                    fragment = new ProfileFragment();
+                }else if (id == R.id.nav_chat){
+
+                }else if (id == R.id.nav_contact){
+
+                }else if (id == R.id.nav_online){
+
+                }else if (id == R.id.nav_logout){
+
+                }
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container_wrapper, fragment);
+                transaction.commit();
+
+                DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+                assert drawerLayout != null;
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    private void disableNavigationViewScrollbars(NavigationView navigationView) {
+        if (navigationView != null) {
+            NavigationMenuView navigationMenuView = (NavigationMenuView) navigationView.getChildAt(0);
+            if(navigationMenuView != null){
+                navigationMenuView.setVerticalScrollBarEnabled(false);
+            }
+        }
+    }
 }
--- app/src/main/java/some/weird/ppafirebaselogin2017/ProfileFragment.java
+++ app/src/main/java/some/weird/ppafirebaselogin2017/ProfileFragment.java
@@ -1,8 +1,94 @@
 package some.weird.ppafirebaselogin2017;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import some.weird.ppafirebaselogin2017.Helper.Helper;
+import some.weird.ppafirebaselogin2017.Helper.SimpleDividerItemDecoration;
+
 /**
  * Created by artur on 05.09.2017.
  */
 
-public class ProfileFragment {
+public class ProfileFragment extends Fragment {
+    private static final String TAG = ProfileFragment.class.getSimpleName();
+    private ImageView profilePhoto;
+    private TextView profileName;
+    private TextView country;
+    private TextView userStatus;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private String id;
+    private static final int REQUEST_READ_PERMISSION = 120;
+
+    public ProfileFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        getActivity().setTitle("My Profile");
+
+        profileName = (TextView) view.findViewById(R.id.profile_name);
+        country = (TextView) view.findViewById(R.id.country);
+        profileName.setVisibility(View.GONE);
+        country.setVisibility(View.GONE);
+
+        profilePhoto = (ImageView) view.findViewById(R.id.circleView);
+        profilePhoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                final Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, Helper.SELECT_PICTURE);
+            }
+        });
+        recyclerView = (RecyclerView)view.findViewById(R.id.profile_list);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addItemDecoration(new SimpleDividerItemDecoration(getActivity()));
+
+        ((FirebaseApplication)getActivity().getApplication()).getFirebaseAuth();
+        id = ((FirebaseApplication)getActivity().getApplication()).getFirebaseUserAuthenticateId();
+        FirebaseDatabaseHelper firebaseDatabaseHelper = new FirebaseDatabaseHelper();
+        firebaseDatabaseHelper.isUserKeyExist(id, getActivity(), recyclerView);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.edit_profile, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_edit_profile){
+            Intent editProfileIntent = new Intent(getActivity(), EditProfileActivity.class);
+            getActivity().startActivity(editProfileIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
