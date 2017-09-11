67ccee06b2ee19c0203af882bc3d6114d2c2a76e
Artur Perec <artur.g.perec@gmail.com>
2017-09-11 23:32:27 +0200

    Changed color of buttons, Added Activity Edit profile layout, started to write Edit Profile Activity

--- app/src/main/java/some/weird/ppafirebaselogin2017/EditProfileActivity.java
+++ app/src/main/java/some/weird/ppafirebaselogin2017/EditProfileActivity.java
@@ -1,8 +1,28 @@
 package some.weird.ppafirebaselogin2017;
 
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * Created by artur on 05.09.2017.
  */
 
 public class EditProfileActivity {
+
+    private static final String TAG = EditProfileActivity.class.getSimpleName();
+    private EditText editProfileName;
+    private EditText editProfileCountry;
+    private EditText editProfilePhoneNumber;
+    private EditText editProfileHobby;
+    private EditText editProfileBirthday;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
 }
